8d0e9b39089a1d68a8be3fc61bee981ab2d1980e
Rhazarian <Rhazarian@ya.ru>
2019-01-26 13:52:32 +0300

    more tests

--- function_testing.cpp
+++ function_testing.cpp
@@ -1,7 +1,7 @@
 #include "function.h"
 #include "gtest/gtest.h"
 
-TEST(correctess, simple_lambda)
+TEST(correctness, simple_lambda)
 {
     function<int(int)> func = [](int a) {
         return a + 10;
@@ -15,13 +15,13 @@ namespace {
     }
 }
 
-TEST(correctess, simple_func_ptr)
+TEST(correctness, simple_func_ptr)
 {
     function<int(int)> func = test_a;
     EXPECT_EQ(20, func(10));
 }
 
-TEST(correctess, assigment)
+TEST(correctness, assigment)
 {
     function<int(int)> func_a = test_a;
     function<int(int)> empty;
@@ -29,14 +29,14 @@ TEST(correctess, assigment)
     EXPECT_EQ(20, empty(10));
 }
 
-TEST(correctess, assigment_func_ptr)
+TEST(correctness, assigment_func_ptr)
 {
     function<int(int)> func;
     func = test_a;
     EXPECT_EQ(20, func(10));
 }
 
-TEST(correctess, bool_conversion)
+TEST(correctness, bool_conversion)
 {
     function<int(int)> func_empty;
     EXPECT_EQ(false, !!func_empty);
@@ -44,7 +44,7 @@ TEST(correctess, bool_conversion)
     EXPECT_EQ(true, !!func);
 }
 
-TEST(correctess, move)
+TEST(correctness, move)
 {
     function<int(int)> func_a = test_a;
     EXPECT_EQ(true, !!func_a);
@@ -52,4 +52,32 @@ TEST(correctess, move)
     EXPECT_EQ(false, !!func_a);
     EXPECT_EQ(true, !!func);
     EXPECT_EQ(20, func(10));
+}
+
+TEST(correctness, assign_fptr_to_lambda)
+{
+    function<int(int)> func = [](int a) {
+        return a + 10;
+    };
+    EXPECT_EQ(20, func(10));
+    func = test_a;
+    EXPECT_EQ(20, func(10));
+}
+
+TEST(correctness, assign_labmda_to_fptr)
+{
+    function<int(int)> func = test_a;
+    EXPECT_EQ(20, func(10));
+    func = [](int a) {
+        return a + 10;
+    };
+    EXPECT_EQ(20, func(10));
+}
+
+TEST(correctness, assign_nullptr_to_fptr)
+{
+    function<int(int)> func = test_a;
+    EXPECT_EQ(20, func(10));
+    func = nullptr;
+    EXPECT_EQ(false, !!func);
 }
